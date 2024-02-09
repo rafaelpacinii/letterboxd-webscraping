b0440cd12712448bcae1ed327b73a4b4671a0532
Rafael Pacini <105872935+rafaelpacinii@users.noreply.github.com>
2024-02-09 20:56:07 +0000

    Second part semi-complited, needs to add xpath list

--- R/main.r
+++ R/main.r
@@ -1,22 +1,42 @@
-# First part: getting the path to the movie page ----------------------
-url_movies_list <- "https://letterboxd.com/films/ajax/popular/page/"
+# First part: getting the paths to the movies page --------------------
+url_base <- "https://letterboxd.com"
 user_agent <- "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3"
 num_pages <- 100
 
-req_base <- httr2::request(url_movies_list) |>
+req_base_pl <- httr2::request(url_base) |>
   httr2::req_user_agent(user_agent) |>
+  httr2::req_url_path(path = "/films/ajax/popular/page/") |>
   httr2::req_url_query(`esiAllowFilters` = "true")
 
-reqs <- purrr::map(1:num_pages, ~ {
-  req_base |>
-    httr2::req_url_path_append(`page_number` = glue::glue("{.x}/"))
+reqs_pl <- purrr::map(1:num_pages, ~ {
+  req_base_pl |>
+    httr2::req_url_path_append(page_number = glue::glue("{.x}/"))
 })
 
-resps <- httr2::req_perform_parallel(reqs, progress = TRUE, on_error = "continue")
+resps_pl <- httr2::req_perform_parallel(reqs_pl, progress = "FP: responses", on_error = "continue")
 
-movies <- purrr::map_dfr(resps, ~ {
+movies_path_list <- purrr::map(resps_pl, ~ {
   httr2::resp_body_html(.x) |>
     rvest::html_elements(".film-poster") |>
-    rvest::html_attr("data-target-link") |>
-    data.frame()
+    rvest::html_attr("data-target-link")
+}, .progress = "FP: movies_path_list")
+
+movies_path <- unlist(movies_path_list)
+
+# Second part: getting the data from each movie page ------------------
+reqs_ml <- purrr::map(movies_path, ~ {
+  httr2::request(url_base) |>
+    httr2::req_user_agent(user_agent) |>
+    httr2::req_url_path(path = .x)
 })
+
+resps_ml <- httr2::req_perform_parallel(reqs_ml, progress = "SP: responses", on_error = "continue")
+
+movies_data <- purrr::map(resps_ml, ~ {
+  movie_data <- list(
+    nome = httr2::resp_body_html(.x) |>
+      rvest::html_elements(".cast-list .text-slug") |>
+      rvest::html_text() |>
+      paste0(collapse = ", ")
+  )
+}, .progress = "SP: movies_data")
